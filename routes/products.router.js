0903d1154ce1969fc050f0671cc4c4d021f89cbd
Daniel Hernandez <daniher02@gmail.com>
2023-02-15 16:49:31 -0300

    clase 17

--- routes/products.router.js
+++ routes/products.router.js
@@ -5,44 +5,51 @@ const router = express.Router();
 const service = new ProductsService();
 
 
-router.get('/', (req, res) => {
-  const products = service.find();
+router.get('/',async (req, res) => {
+  const products = await service.find();
   res.json(products);
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const {id } = req.params
-  const product = service.findOne(id);
+  const product = await service.findOne(id);
 
   res.json(product);
 
 })
 
-router.post('/', (req,res) => {
+router.post('/', async (req,res) => {
   const body = req.body;
-  const newProduct = service.create(body)
+  const newProduct = await service.create(body)
   res.status(201).json({
     message: 'creado',
     data: newProduct
   })
 })
 
-router.patch('/:id', (req, res) => {
-  const {id } = req.params;
-  const body = req.body;
-  const product = service.update(id, body)
-  res.json(
-    {
-      id,
-      message: 'update',
-      data: product
-    }
-  )
+router.patch('/:id', async (req, res) => {
+  try{
+    const {id } = req.params;
+    const body =  req.body;
+    const product = await service.update(id, body)
+    res.json(
+      {
+        id,
+        message: 'update',
+        data: product
+      }
+    )
+  }catch (error){
+    res.status(404).json({
+      message: error.message
+    })
+  }
+
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const {id } = req.params;
-  const product_delete = service.delete(id)
+  const product_delete = await service.delete(id)
   res.json(
     {
       id,
