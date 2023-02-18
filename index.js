8e53236b24815ad903c788a534856fcce29a865d
Daniel Hernandez <daniher02@gmail.com>
2023-02-18 19:42:43 -0300

    clas 24

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require("cors");
 
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler')
 
@@ -7,6 +8,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+const whilelist = ['http://localhost', 'https://myapp.cl'] // ? rutas de las cuales SI quiero recibir peticiones
+const options = {
+  origin: (origin, callback) => {
+    if (whilelist.includes(origin)){
+      callback(null, true);
+    }else{
+      callback(new Error('no permitido'))
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
